d93071774fc2cce0fa35e6ccd70efa83981009d8
BelleNerissa <49173787+BelleNerissa@users.noreply.github.com>
2022-02-13 18:27:22 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ export default function PaginaInicial() {
               marginBottom: "32px",
             }}
           >
-            <Titulo tag="h2">Boas vindas de volta, aventureiro!</Titulo>
+            <Titulo tag="h2">Boas vindas, aventureiro!</Titulo>
             <Text
               variant="body3"
               styleSheet={{
